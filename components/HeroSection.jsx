2161151f6fa5beaaf3d5426a72d38f65726035c4
Mrwhiteblack9-hub <ahmadalikhlas9@gmail.com>
2025-08-29 16:53:55 +0100

    HeroSection.jsx

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -2,7 +2,7 @@
 import Link from 'next/link'
 import Countdown from './Countdown'
 
-export default function HeroSection({ buyUrl, lang = 'fr', dictHero = {}, banner = '/banner.jpg' }) {
+export default function HeroSection({ buyUrl, lang = 'fr', dictHero = {}, banner = '/banner.webp' }) {
   // target: 13 September 2025 00:00:00 UTC
   const targetISO = '2025-09-13T00:00:00Z'
 
